3989fc75d859ee64e1bf56c6c88f3f4b48fe255c
montsemsanz <montsemsanz@gmail.com>
2021-03-05 16:49:52 +0100

    Primer commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Main
+{
+
+    /**
+     *  
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world Hola mundo");
+
+        int a = 10;
+		int b = 5;
+
+    }
+}
